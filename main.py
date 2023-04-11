de5a822aaedb9d16a7874d5f4b636e2195702150
Luca <116733069+luqqa26@users.noreply.github.com>
2023-04-11 16:47:21 +0200

    fast fertig mit eingabe

--- main.py
+++ main.py
@@ -1,35 +1,5 @@
 from typing import List, Dict, Optional
-
-
-def add_new_cable():
-    cable_sequence = input("At which position is the cable: ")
-    color = input("What is the color: ")
-    cable_module[cable_sequence] = color
-    print("Saved input", cable_sequence, color)
-
-
-def show_module():
-    cable_sequence = input("At which point is the cable: ")
-    if name in cable_module:
-        print(cable_sequence, ":", cable_module[cable_sequence])
-    else:
-        print("Error no input found:", cable_sequence)
-
-    while True:
-        print("\nCable Menu:")
-        print("1. Add Cable")
-        print("2. Show Module")
-        print("3. Stop")
-
-        choice = input("Your choice: ")
-        if choice == "1":
-            add_new_cable()
-        elif choice == "2":
-            show_module()
-        elif choice == "3":
-            break
-        else:
-            print("Invalid input. Please select 1, 2 or 3.")
+import sys
 
 
 class Wire:
@@ -52,38 +22,96 @@ class Module:
     def getCablesequence(self):
         return self.__sequence
 
+def add_new_cable():
+    cable_sequence = input("At which position is the cable: ")
+    color = input("What is the color: ")
+    cable_module[cable_sequence] = color
+    print("Saved input", cable_sequence, color)
 
-def defuseModule(module):
-    pass
+
+def show_module():
+    print("You can now start with typing the serialnumber and wires")
+
+    while True:
+        print("\nWire Menu:")
+        print("1. Input serialnumber and wires")
+        print("2. Stop the program")
+
+        choice = input("Your choice: ")
+        if choice == "1":
+            defuseModule()
+            break
+        elif choice == "2":
+            print("Program terminated.")
+            sys.exit()
+        else:
+            print("Invalid input. Please select 1, 2 or 3.")
+
+def getSerialNumber():
+    # User input for serial number
+    serial_number = input("Enter serial number: ")
+
+    # Check if serial number is valid
+    if not serial_number.isdigit():
+        print("Error: Invalid serial number. Please enter a number.")
+        return None
+
+    # Convert serial number to integer
+    serial_number = int(serial_number)
+
+    # Check if serial number is within valid range
+    if not (1 <= serial_number <= 999):
+        print("Error: Serial number must be between 1 and 999.")
+        return None
+
+    return serial_number
 
 
-def test():
-    pass
+def defuseModule():
+    # User input 
+    serial_number = getSerialNumber()
+    colors = ["red", "blue", "yellow", "white", "black"]
+    input_str = input("Enter wire names separated by commas: ")
+    wire_names = list(map(str.strip, input_str.split(",")))
+
+      # User input for serial number
+    if serial_number is None:
+        return None
+    
+    # Create wire objects and add them to wireModule
+    wireModule = Module(cable_quantity = 0, serialnumber=serial_number)  # Instance
+    for name in wire_names:
+        if name == "wire1":
+            wire = Wire(color=colors[0])
+        elif name == "wire2":
+            wire = Wire(color=colors[1])
+        elif name == "wire3":
+            wire = Wire(color=colors[2])
+        elif name == "wire4":
+            wire = Wire(color=colors[3])
+        elif name == "wire5":
+            wire = Wire(color=colors[4])
+        elif name == "wire6":
+            wire = Wire(color=colors[0])
+        else:
+            print(f"Error: Invalid wire name '{name}'. Skipping...")
+            continue
+        
+        wireModule.addcableobject(cable=wire)
+    
+    return wireModule
 
 
 if __name__ == "__main__":
-    # test()
-    print("Start program")
+    show_module()
     colors = ["red", "blue", "yellow", "white", "black"]
-
-    wire1 = Wire(color=colors[0])
-    wire2 = Wire(color=colors[3])
-    wire3 = Wire(color=colors[4])
-    wire4 = Wire(color=colors[4])
-    wire5 = Wire(color=colors[1])
-    wire6 = Wire(color=colors[4])
-
-    wireModule = Module(cable_quantity=0, serialnumber=0)  # Instance
-    wireModule.addcableobject(cable=wire1)
-    wireModule.addcableobject(cable=wire2)
-    wireModule.addcableobject(cable=wire3)
-    wireModule.addcableobject(cable=wire4)
-    wireModule.addcableobject(cable=wire5)
-    wireModule.addcableobject(cable=wire6)
-
+    wireModule = defuseModule()
+    print("Start program")
+   
     cablelist = wireModule.getCablesequence()
     print(cablelist)
-    # 3 Kabel:
+    
+    # 3 Wire:
     if len(cablelist) == 3:
         if cablelist[0].color != colors[0]:
             print("Cut second wire")
@@ -94,7 +122,8 @@ if __name__ == "__main__":
         else:
             print("Cut last wire")
 
-    # 4 Kabel:
+    
+    # 4 Wire:
     if len(cablelist) == 4:
         if sum([1 for cable in cablelist if cable.color == colors[0]]) > 1 and wireModule.serial % 2 == 1:
             print("Cut last red wire")
@@ -107,7 +136,7 @@ if __name__ == "__main__":
         else:
             print("Cut second wire")
 
-    # 5 Kabel:
+    # 5 Wire:
     if len(cablelist) == 5:
         if cablelist[-1] == colors[4] and wireModule.serial % 2 == 1:
             print("Cut fourth wire")
@@ -118,7 +147,7 @@ if __name__ == "__main__":
         else:
             print("Cut first wire")
 
-    # 6 Kabel:
+    # 6 Wire:
     if len(cablelist) == 6:
         if not(colors[2] in cablelist) and wireModule.serial % 2 == 1:
             print("Cut third wire")
